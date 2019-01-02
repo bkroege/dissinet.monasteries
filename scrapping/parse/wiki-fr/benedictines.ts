2f0f76784dfe57a21fb0b00146dd84fff5281f99
adammertel <mertel.adam@gmail.com>
2019-01-02 18:58:46 +0100

    parsing benedictines wiki

--- scrapping/parse/wiki-fr/benedictines.ts
+++ scrapping/parse/wiki-fr/benedictines.ts
@@ -2,26 +2,106 @@ var request = require("request");
 var cheerio = require("cheerio");
 var fs = require("fs");
 import { WikiFrParser } from "./parser";
-import { Monastery } from "./../monastery";
+import Base from "./../base";
 
 export class benedictinesWikiFrParer extends WikiFrParser {
   initialiseRecords(next) {
-    const response = [];
     request(this.meta.url, (err, resp, html) => {
       if (!err) {
         const $ = cheerio.load(html);
 
         $("table.wikitable").map((ri, table) => {
-          if (ri > -1) {
-            $(table)
-              .find("tr")
-              .map((ti, row) => {
-                response.push(this.monasteryCreator(row));
-              });
+          $(table)
+            .find("tr")
+            .map((ti, record) => this.addMonastery(record));
+        });
+      }
+      next();
+    });
+  }
+
+  parseMonastery(monastery, next) {
+    const $ = cheerio.load(monastery.html);
+    $("td").map((ci, column) => {
+      if (ci === 0) {
+        // name
+        monastery.setName($(column).text());
+
+        // link
+        const aEl = $(column).find("a");
+        if (aEl.attr("class") !== "new") {
+          const link = this.meta.rootUrl + aEl.attr("href");
+          monastery.setLink(link);
+        }
+      }
+
+      // gender
+      if (ci === 1) {
+        const genderText = $(column)
+          .not("sup > *")
+          .text();
+
+        let gender = "";
+        let genderNote = "";
+        if (genderText) {
+          if (
+            genderText.includes("moines") &&
+            genderText.includes("moniales")
+          ) {
+            genderNote = genderText;
+            gender = "d";
+          } else if (genderText === "moines") {
+            gender = "m";
+          } else if (genderText === "moniales") {
+            gender = "n";
+          } else {
+            genderNote = genderText;
+          }
+        }
+
+        monastery.setGender(genderNote, gender);
+      }
+
+      // orders
+      if (ci === 3) {
+        const dateText = Base.cleanText($(column).text(), {
+          chars: ["\n"],
+          trim: true
+        });
+
+        const occurences = dateText.split(", puis");
+
+        occurences.map(occurence => {
+          const parts = occurence.split("-");
+
+          if (parts.length === 2) {
+            const parsedFrom = parseInt(parts[0]) || false;
+            const parsedTo = parseInt(parts[1]) || false;
+            monastery.addOrder({
+              name: "benedictines",
+              from: parsedFrom,
+              to: parsedTo,
+              fromNote:
+                parts[0] == parsedFrom
+                  ? ""
+                  : Base.cleanText(parts[0], { trim: false }),
+              toNote:
+                parts[1] == parsedTo
+                  ? ""
+                  : Base.cleanText(parts[1], { trim: false }),
+              note: ""
+            });
+          } else {
+            monastery.addEmptyOrder(occurence);
           }
         });
       }
-      next(response);
+    });
+
+    this.inspectWikiPage(monastery, () => {
+      monastery.finishParsing();
+      monastery.save(this.store);
+      next();
     });
   }
 }
