72c3734df627e60e7f997a5825674ce59fa104c7
adammertel <mertel.adam@gmail.com>
2019-04-10 14:21:35 +0200

    cisterciens darmc transitioned gender parsing

--- scrapping/parse/darmc/cisterciennes.ts
+++ scrapping/parse/darmc/cisterciennes.ts
@@ -1,5 +1,6 @@
 import { DarmcParser } from "./parser";
 import BASE from "./../../base";
+import { inflate } from "zlib";
 
 export class cisterciennesDarmcParser extends DarmcParser {
   parseMonastery(monastery, next) {
@@ -18,8 +19,39 @@ export class cisterciennesDarmcParser extends DarmcParser {
       to: html.D_DATE
     });
 
+    const notes = monastery.html.NOTES;
+    const genderCode = monastery.html.GENDER;
+    if (genderCode === "T") {
+      if (notes) {
+        const firstGender = notes.includes("ounded as male") ? "M" : "F";
+        const secondGender = firstGender === "M" ? "F" : "M";
+
+        const noteChunks = notes.split(";");
+        const changeYear = parseInt(
+          noteChunks[noteChunks.length - 1].match(/\d+$/),
+          10
+        );
+
+        console.log(
+          "changeYear",
+          noteChunks[noteChunks.length - 1],
+          changeYear
+        );
+
+        const firstDate = BASE.timeParse({ from: html.F_DATE, to: changeYear });
+        const secondDate = BASE.timeParse({
+          from: changeYear,
+          to: html.D_DATE
+        });
+
+        monastery.addOrder({ gender: firstGender }, firstDate);
+        monastery.addOrder({ gender: secondGender }, secondDate);
+      }
+    } else {
+      monastery.addOrder({ gender: genderCode }, time);
+    }
+
     monastery.addStatus({}, time);
-    monastery.addOrder({ gender: monastery.html.GENDER }, time);
 
     if (html.CITATION) {
       monastery.setParam("sourceliterature", html.CITATIOn);
